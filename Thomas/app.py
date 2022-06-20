8f0300499edcf7fdea3ddcb0d031e3d407f7bf9a
ErikRyg <erikrygiel@gmx.de>
2022-06-20 10:32:30 +0200

    added siames_network.py

--- Thomas/app.py
+++ Thomas/app.py
@@ -45,7 +45,7 @@ loaded_datasets = {}
 app.layout = html.Div([
 
 
-    
+
     dbc.Navbar(
         [
             html.A(
@@ -56,7 +56,7 @@ app.layout = html.Div([
                         dbc.Col(dbc.NavbarBrand("PPR Hausaufgaben Cheatingtool", className="ml-2")),
                     ],
                     align="center",
-                    no_gutters=True,
+                    # no_gutters=True,
                 ),
                 href="https://www.ni.tu-berlin.de/menue/members/postgraduate_students_and_doctoral_candidates/goerttler_thomas/",
             ),
@@ -65,14 +65,14 @@ app.layout = html.Div([
         dark=True,
         id = "navbar",
     ),
-    
-    
+
+
     # content will be rendered in this element
     html.Div([
-        
+
         dbc.Row(
                 [
-                    
+
                     dbc.Col(html.Div([
                         dbc.FormGroup(
                             [
@@ -81,7 +81,7 @@ app.layout = html.Div([
                                 dcc.Upload(
                                     id="upload-data",
                                     children=html.Div(
-                                        ["Drag file"]
+                                        ["Drag file (i.e. '...-Antworten.csv)"]
                                     ),
                                     style={
                                         "width": "100%",
@@ -158,7 +158,7 @@ app.layout = html.Div([
                         ),
 
 
-                    ], 
+                    ],
                     className="h-100"),md=3, className="sidebar"),
                     dbc.Col(html.Div([
 
@@ -189,49 +189,49 @@ app.layout = html.Div([
                                dbc.Col(html.Div(id='answer_2'), md=4, className="graph_container"),
                                dbc.Col(dcc.Graph(id='student_comparision'), md=4, className="graph_container"),
                         ]),
-                    ], 
+                    ],
                     className="h-100"),md=9, className="content")
                 ], className="h-100"
         ),
-        
-        
+
+
     ],
         id='page-content',
         className="h-100"
     )
 
-], className="h-100")    
-    
+], className="h-100")
+
 
 
 
 @app.callback(
     [
         Output('data_picker', 'options'),
-        Output('data_picker', 'value'),   
+        Output('data_picker', 'value'),
     ],
     [
         Input('upload-data', 'filename'),
         Input("upload-data", "contents"),
     ])
 def update_dropdown(uploaded_filename, uploaded_file_content):
-    
+
     if uploaded_file_content != None:
-    
+
         content_type, content_string = uploaded_file_content.split(',')
         decoded = base64.b64decode(content_string)
-        
+
         try:
             if 'csv' in uploaded_filename:
                 data = Data(io.StringIO(decoded.decode('utf-8')))
-                
+
             loaded_datasets[uploaded_filename] = data
-            
+
         except Exception as e:
-            print(e)    
+            print(e)
 
         print("Update data")
-    
+
     else:
         return [], None
 
@@ -246,26 +246,26 @@ def update_dropdown(uploaded_filename, uploaded_file_content):
         Output('exercise_picker', 'value'),
         Output('exercise_picker_sum', 'options'),
         Output('exercise_picker_sum', 'value'),
-        
+
         Output('student_1_picker', 'options'),
         Output('student_1_picker', 'value'),
         Output('student_2_picker', 'options'),
         Output('student_2_picker', 'value'),
     ],
     [
-        Input('data_picker', 'value'), 
+        Input('data_picker', 'value'),
     ])
 def update_dropdown(data_picker):
-    
+
     print("Data Picker was updated.")
-    
+
     if data_picker != None:
         print(f"Change dataset to {data_picker}")
         data = loaded_datasets[data_picker]
     else:
         print("return pass")
         return [], None, [], None, [], None, [], None
-    
+
     return  [{'label': i, 'value': i} for i in data.col_names + [AGGREGATION]], AGGREGATION, [{'label': i, 'value': i} for i in data.col_names], data.col_names, [{'label': i, 'value': i} for i in data.names], data.names[0], [{'label': i, 'value': i} for i in data.names], data.names[1]
 
 
@@ -281,21 +281,21 @@ def update_dropdown(data_picker):
         Input('student_1_picker', 'value'),
         Input('student_2_picker', 'value'),
         Input('exercise_picker', 'value'),
-        Input('data_picker', 'value'), 
+        Input('data_picker', 'value'),
     ],)
 def update_output(student1, student2, exercise_picker, data_picker):
-    
+
     print("Update student comparision")
-    
+
     if data_picker == None or exercise_picker == None:
         return "", "", {}
-    
+
     data = loaded_datasets[data_picker]
-    
-    
+
+
     sims = [data.similarities[key].at[student1, student2] for key in data.similarities.keys()]
     print(data.similarities.keys(), sims)
-    
+
     figure={
             'data': [
                 {'x': list(data.similarities.keys()), 'y':sims, 'type': 'bar'},
@@ -306,14 +306,14 @@ def update_output(student1, student2, exercise_picker, data_picker):
         }
     if exercise_picker == AGGREGATION:
         return "", "", figure
-    
+
     answer1 = answer2 = ""
     if student1 != None:
         answer1 = data.submissions.at[student1, exercise_picker]
     if student2 != None:
         answer2 = data.submissions.at[student2, exercise_picker]
-    
-  
+
+
     return answer1, answer2, figure
 
 @app.callback(
@@ -331,21 +331,21 @@ def update_output(student1, student2, exercise_picker, data_picker):
         Input('student_2_picker', 'value'),
     ],)
 def update_figure(data_picker, exercise_picker, exercise_picker_sum, options_picker, student1, student2):
-    
+
     print("Update similarities")
     print(data_picker, exercise_picker)
-    
+
     if exercise_picker == None or data_picker == None:
         return {}, {}, []
-    
+
     data = loaded_datasets[data_picker]
-    
+
     if exercise_picker == AGGREGATION:
-        
-        
+
+
         if exercise_picker_sum == None:
             return {}, {}, []
-        
+
         keys = exercise_picker_sum
         r = data.similarities[keys[0]] - data.similarities[keys[0]]
         for key in keys:
@@ -354,7 +354,7 @@ def update_figure(data_picker, exercise_picker, exercise_picker_sum, options_pic
     else:
         r = data.similarities[exercise_picker]
 
-    
+
     try:
         print(r["Vorlage, Die"].sort_values())
     except:
@@ -368,15 +368,15 @@ def update_figure(data_picker, exercise_picker, exercise_picker_sum, options_pic
 
             indeces_empty = data.get_not_empty_indices(exercise_picker)
             indeces = list(set(indeces).intersection(set(indeces_empty)))
-            
+
         if "template" in options_picker:
             try:
                 indeces_non_teample = data.get_non_similar_to_template(exercise_picker)
                 indeces = list(set(indeces).intersection(set(indeces_non_teample)))
             except:
                 print("No template defined")
-            
-            
+
+
         r = r.loc[indeces, :]
         r = r[indeces]
 
@@ -390,33 +390,33 @@ def update_figure(data_picker, exercise_picker, exercise_picker_sum, options_pic
     rdm_fig.update_yaxes(showticklabels=False)
 
     start = 0
-    
+
     embedding = MDS(n_components=2)
-    X_transformed = embedding.fit_transform(r) 
-    
+    X_transformed = embedding.fit_transform(r)
+
     X_transformed = np.round(X_transformed, 2)
-   
-    
+
+
     df = pd.DataFrame(X_transformed,columns=['x','y'], index = r.index)
-   
+
     df['text'] = df.index
     df['same'] = df.groupby(['x','y'])['text'].transform(lambda x: '<br>'.join(x))
     counted_X = df.groupby(['x','y','same']).count().reset_index()
-    
+
     counted_X = counted_X.rename(columns={'text': 'size'})
     counted_X = counted_X.rename(columns={'same': 'text'})
-    
+
     counted_X['color'] = 1
     counted_X['color'][counted_X['text'].str.contains(student1)] = 0
     counted_X['color'][counted_X['text'].str.contains(student2)] = 0
     counted_X['size'] = 5 + 3 * np.log(counted_X['size'])
 
-   
+
     unq, count = np.unique(X_transformed, axis=0, return_counts=True)
     X_transformed = X_transformed + np.random.normal(0, 0.001, size = X_transformed.shape)
 
 
-    
+
 
     mds_fig = go.Figure()
     mds_fig.add_trace(go.Scatter(x=counted_X['x'], y=counted_X['y'], mode='markers', text = counted_X['text'],
@@ -425,10 +425,10 @@ def update_figure(data_picker, exercise_picker, exercise_picker_sum, options_pic
                                  #         "reversescale": True, "showscale": True,
                                  #         "line": {"width": 1, "color": 'black'}},
                                  # line={"width": 1, "color": 'black'}, name=f"{model}", text=text))
-                                 
+
                                  #"color": 'rgba(135, 206, 250, 0.5)',
-    
-    
+
+
     mds_fig.update_layout(height=300, margin=GLOBAL_MARGIN, showlegend=False,
                           xaxis={"title": "MDS dim 12", "scaleanchor": "y", "scaleratio": 1},
                           yaxis={"title": "MDS dim 2"}, template=GLOBAL_TEMPLATE)
@@ -437,14 +437,14 @@ def update_figure(data_picker, exercise_picker, exercise_picker_sum, options_pic
     upper = r.where(np.triu(np.ones(r.shape), 1).astype(np.bool))
     upper = upper.stack().reset_index()
     upper.columns = ['Student:in 1','Student:in 2','Ähnlichkeit']
-    
-    
-    
+
+
+
     upper = upper.sort_values(by='Ähnlichkeit', ascending=False)
 
     return rdm_fig, mds_fig, upper.to_dict('records')
-    
-    
+
+
 
 
 
