fd0bdbf97e4fcf88d4c02de9605cedded98f1bcf
ErikRyg <erikrygiel@gmx.de>
2022-11-17 02:41:45 +0100

    add handmade plagiate

---
+++ data/labled/handmade_plagiate_2_1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  char sammlung[100] = "";
+  int counter = 0;
+  int k = 1;
+  while (argc > k)
+  {
+    char *p = argv[k];
+    while (!((*p) == '\0'))
+    {
+      if (!((*p == 'x') || (*p == 'y') || (*p == 'q') || (*p == 'X') || (*p == 'Y') || (*p == 'Q')))
+      {
+        counter++;
+        sammlung[counter - 1] = *p;
+      }
+      p++;
+    }
+    k++;
+  }
+  int l = 0;
+  char neuerString[100] = "";
+  sammlung[counter] = '\0';
+  printf("x,y,q und X,Y,Q aussortiert: %s\n", sammlung);
+  while (counter > l)
+  {
+    neuerString[l / 3] = sammlung[l];
+    l += 3;
+  }
+  neuerString[l / 3] = '\0';
+  printf("Der neue String lautet: %s \n", neuerString);
+}
